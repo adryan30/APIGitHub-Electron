ea6d4fd978551ac35f73033a9d7d096d76f3f450
Adryan Almeida <adryan1990@gmail.com>
2020-01-17 21:11:51 -0300

    Initial Structure

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+  },
+  env: {
+    browser: true,
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
+  },
+};
---
+++ src/routes.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+// import { Login, Dashboard, New } from './pages';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch />
+    </BrowserRouter>
+  );
+}
